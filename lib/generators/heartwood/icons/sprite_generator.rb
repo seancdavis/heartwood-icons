17e4743c2633a98cd2346e54e23a1e71c5355dd9
Sean C Davis <scdavis41@gmail.com>
2018-06-01 07:53:53 -0400

    Fix sprite indentation

--- lib/generators/heartwood/icons/sprite_generator.rb
+++ lib/generators/heartwood/icons/sprite_generator.rb
@@ -15,6 +15,11 @@ module Heartwood
         @svg_content = ''
       end
 
+      # Step through every SVG file in the app/assets/images/icons directory in
+      # the rails app.
+      #
+      # We only look at top-level "g" elements, and strip their interfering
+      # attributes.
       def process_icons
         icons_dir = Rails.root.join('app', 'assets', 'images', 'icons')
         tmpl = File.read(File.expand_path("./templates/icon.svg.erb", __dir__))
@@ -26,15 +31,18 @@ module Heartwood
         end
       end
 
+      # Wrap our SVG icon markup in one SVG/XML document.
       def wrap_icons
         tmpl = File.read(File.expand_path('./templates/icon_wrapper.svg.erb', __dir__))
         @icons_sprite = ERB.new(tmpl).result(binding)
       end
 
+      # Fix indentation in sprite markup.
       def fix_indentation
-        @icons_sprite = Nokogiri::XML(@icons_sprite).to_xml
+        @icons_sprite = Nokogiri::XML(@icons_sprite, &:noblanks).to_xml
       end
 
+      # Write the sprite to file in the Rails app (app/assets/images/icons.svg).
       def write_sprite
         dest = Rails.root.join('app', 'assets', 'images', 'icons.svg')
         template 'icons_sprite.svg.erb', dest, force: true
@@ -42,6 +50,8 @@ module Heartwood
 
       private
 
+        # Strips attributes that tend to interfere with making the sprite render
+        # properly and helps to give the developer more control over the styles.
         def strip_attrs(doc)
           %w{id stroke stroke-width fill fill-rule transform}.each do |attr|
             doc.css("[#{attr}]").each { |n| n.delete(attr) }
@@ -52,38 +62,3 @@ module Heartwood
     end
   end
 end
-
-
-
-###########
-
-# require 'rexml/document'
-
-# desc 'Combine all SVG icons into a single SVG'
-# task :build_svg do
-#   icons_dir = Rails.root.join('app', 'assets', 'images', 'icons')
-
-#   # rubocop:disable Metrics/LineLength
-#   svg_content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
-# <svg width=\"256px\" height=\"256px\" viewBox=\"0 0 256 256\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">"
-#   # rubocop:enable Metrics/LineLength
-
-#   Dir.glob("#{icons_dir}/*.svg").each do |file|
-#     doc = Nokogiri::XML(File.read(file))
-#     icon_name = doc.css('#Page-1 > g')[0]['id']
-#     content = doc.css('#Page-1 g').to_s
-#     # rubocop:disable Metrics/LineLength
-#     svg_content += "<svg width=\"256px\" height=\"256px\" viewBox=\"0 0 256 256\" id=\"#{icon_name}\">\
-#   #{content.gsub(/[a-z\-]+=\"[a-zA-Z0-9\-\#]+\"/, '')}\
-# </svg>"
-#     # rubocop:enable Metrics/LineLength
-#   end
-#   svg_content += '</svg>'
-
-#   svg_content = REXML::Document.new(svg_content)
-#   output = ''
-#   svg_content.write(output, 2)
-
-#   icons_file = Rails.root.join('app', 'assets', 'images', 'icons.svg')
-#   File.open(icons_file, 'w+') { |file| file.write(output) }
-# end
